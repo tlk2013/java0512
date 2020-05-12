5f89fef2e93618339d327e579385892355e2751d
tlk <tlk@tlk.com>
2020-05-12 22:40:17 +0800

    tijiao

---
+++ java01/src/com/dwsoft/tlk/test/TestMain.java
@@ -0,0 +1,17 @@
+package com.dwsoft.tlk.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author tlk
+ * @date 2020/5/12-22:19
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        for (int i = 0; i <100 ; i++) {
+            System.out.println(i);
+        }
+        Map map=new HashMap();
+    }
+}
